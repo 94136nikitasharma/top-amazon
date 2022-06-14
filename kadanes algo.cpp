dcf53ad35f6f9ef01aa9d6a02717854783654032
Nikita Sharma <87696438+94136nikitasharma@users.noreply.github.com>
2022-06-14 15:41:48 +0530

    Create kadanes algo.cpp

---
+++ kadanes algo.cpp
@@ -0,0 +1,16 @@
+class Solution{
+    public:
+   
+    long long maxSubarraySum(int arr[], int n){
+        int sum=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+          sum+=arr[i];  
+          maxi=max(maxi,sum);
+          if(sum<0) sum=0;
+        }
+       
+        return maxi;
+    }
+};
